455f3f163207b61c1b27469fd8edc8869686514d
khimsh <i.khimsh@gmail.com>
2019-10-10 21:59:55 +0400

    Cleaned up the function; added docstring and types.

--- scraper.py
+++ scraper.py
@@ -3,7 +3,13 @@ from selenium import webdriver
 import lxml
 
 
-def get_user(user_email, user_password):
+def get_user(user_email: str, user_password: str) -> dict:
+    """
+    1. Open '/login' in firefox with selenium
+    2. Enter user_email & user_password; Submit
+    3. Open '/profile'; Grab user name & lastname
+    4. Return user name & lastname
+    """
 
     login_url = 'http://applecity.ge/login'
     profile_url = 'http://applecity.ge/profile'
@@ -11,24 +17,29 @@ def get_user(user_email, user_password):
     browser = webdriver.Firefox()
     browser.get(login_url)
 
+    # Select email & password inputs
     username = browser.find_element_by_id("userSigninLogin")
     password = browser.find_element_by_id("userSigninPassword")
+    # Select submit button
     button = browser.find_element_by_xpath('//button[@type=\'submit\']')
 
+    # Enter user creditentials & click submit
     username.send_keys(user_email)
     password.send_keys(user_password)
     button.click()
 
+    # Open profile url & get the response html
+    # If the response contains required data
+    # return user name & lastname with status code 200
+    # If it doesn't return status code 401
+    # Close the browser
     browser.get(profile_url)
-
     soup = BeautifulSoup(browser.page_source, 'lxml')
 
     try:
         name_lastname = soup.find('input', {'id': 'accountName'}).get('value')
-        print(name_lastname)
         return {'status': 200, 'message': name_lastname}
     except AttributeError as err:
-        print(err)
         return {'status': 401, 'message': 'bad creditentials'}
     finally:
         browser.quit()
