e2410bccd60b3c1cb8f016289ab90811560e71e3
khimsh <i.khimsh@gmail.com>
2019-10-11 11:30:39 +0400

    default message more helpful.

--- app.py
+++ app.py
@@ -11,7 +11,7 @@ def index():
         user_password = request.args.get('password')
         return jsonify(get_user(user_email, user_password))
     except:
-        return 'try again'
+        return 'Enter url in following format: ?email=test@testmail.com&password=pass123'
 
 
 if __name__ == '__main__':
